26ea798b22c7a56d77fbd9732222e2628e912fe7
4d4143 <>
2024-05-03 11:05:15 -0300

    Added structure to main function

--- project_1/ip_owner_geo.py
+++ project_1/ip_owner_geo.py
@@ -1,4 +1,7 @@
 import ipaddress
+import argparse
+import sys
+import csv
 
 
 def query_ip():
@@ -14,8 +17,32 @@ def output_findings():
 
 
 def main():
-    pass
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help="Single IP or CIDR range")
+    parser.add_argument("-iL", "--file", help="File with IPs or CIDR ranges")
+    parser.add_argument("-o", "--output", help="Filename for output (In CSV)")
+
+    args = parser.parse_args()
+    variables = vars(args)
+
+    query_results = []
+
+    if variables['ip']:
+        query_results.append(query_ip(variables["ip"]))
+
+    if variables['file']:
+        ips_list = load_file(variables['file'])
+
+        for ip in ips_list:
+            query_results.append(ip)
+
+    if (not variables['ip'] or not variables['file']):
+        print("Please provide an IP or a list of IPs!")
+        sys.exit(0)
+
+    if variables['output']:
+        output_findings(query_results)
 
 if __name__ == '__main__':
     main()
