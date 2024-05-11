c2181488c61a5288ad42cb72b1de4fd4ff8bacce
4d4143 <>
2024-05-11 14:40:25 -0300

    Added pseudostructure

---
+++ project_3/ssh_brute.py
@@ -0,0 +1,12 @@
+
+
+
+#Open and read files containing passwords
+
+#Open connection to SSH server
+
+#Start cycling through password list and attempt combination with provided user
+
+#If valid credentials found, break out of loop and notify user
+
+#If no valid credentials found, notify user
